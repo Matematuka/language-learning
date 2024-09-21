eaade02264de97fb05acb31817aa53890270a232
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-09-21 16:26:22 +0300

    login and registration

---
+++ src/components/ModalRegistration/ModalRegistration.jsx
@@ -0,0 +1,34 @@
+import { useModal } from "../../context";
+import ModalLogin from "../ModalLogin/ModalLogin";
+import RegistrationForm from "../RegistrationForm/RegistrationForm";
+import css from "./ModalRegistration.module.css";
+
+const ModalRegistration = () => {
+  const { openModal } = useModal();
+
+  const handleBtnClick = () => {
+    openModal(<ModalLogin />);
+  };
+
+  return (
+    <div className={css.wrapper}>
+      <div className={css.infoWrapper}>
+        <h3 className={css.title}>Registration</h3>
+        <p className={css.text}>
+          Thank you for your interest in our platform! In order to register, we
+          need some information. Please provide us with the following
+          information
+        </p>
+      </div>
+      <RegistrationForm />
+      <p className={css.textRegister}>
+        Already have an account?
+        <button className={css.registerBtn} onClick={handleBtnClick}>
+          Login now
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default ModalRegistration;
