d4b9524c59bebb89413c34dcec942dd013899ead
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-09-22 17:39:02 +0300

    user menu and review

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,5 +1,31 @@
+import clsx from "clsx";
+import { NavLink } from "react-router-dom";
+import css from "./UserMenu.module.css";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+
 const UserMenu = () => {
-  return <div>UserMenu</div>;
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  const handleActiveLink = ({ isActive }) => {
+    return clsx(css.link, { [css.active]: isActive });
+  };
+
+  return (
+    <nav className={css.nav}>
+      <NavLink to="/" className={handleActiveLink}>
+        Home
+      </NavLink>
+      <NavLink to="/teachers" className={handleActiveLink}>
+        Teachers
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink to="/favorites" className={handleActiveLink}>
+          Favorites
+        </NavLink>
+      )}
+    </nav>
+  );
 };
 
 export default UserMenu;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import css from "./HomePage.module.css";
 import Intro from "../../components/Intro/Intro";
+import Statistics from "../../components/Statistics/Statistics";
 
 const HomePage = () => {
   return (
     <div className={css.container}>
       <Intro />
+      <Statistics />
     </div>
   );
 };
